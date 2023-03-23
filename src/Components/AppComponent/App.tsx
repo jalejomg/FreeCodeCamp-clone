132c5fdf5cb78e3847339a6e0f46d71f827a765c
jalejomg <jalejomg@outlook.com>
2023-03-23 03:51:06 -0500

    Create App component

--- src/Components/AppComponent/App.tsx
+++ src/Components/AppComponent/App.tsx
@@ -1,8 +1,31 @@
+import Data from "../../data/Testimonials";
+import H1Element from "../../Elements/H1Element/H1Element";
+import Testimonial from "../TestimonialComponent/Testimonial";
 import "./App.css";
 
 function App() {
+  const testimonialComponents = Data.map((testimonial, index) => {
+    return (
+      <Testimonial
+        key={index}
+        name={testimonial.name}
+        photoName={testimonial.photoName}
+        country={testimonial.country}
+        position={testimonial.position}
+        company={testimonial.company}
+        testimonial={testimonial.testimonial}
+      />
+    );
+  });
+
   return (
     <>
+      <div className="App">
+        <div className="contenedor-principal">
+          <H1Element contentText="Here is what our alumni say about freeCodeCamp:" />
+          {testimonialComponents}
+        </div>
+      </div>
     </>
   );
 }
