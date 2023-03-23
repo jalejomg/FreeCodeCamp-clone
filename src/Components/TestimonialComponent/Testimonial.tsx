4a0cfeb26d7f74c6d43d8b8c5c2d79ed5e6e4401
jalejomg <jalejomg@outlook.com>
2023-03-23 03:47:40 -0500

    Create testimonial component

---
+++ src/Components/TestimonialComponent/Testimonial.tsx
@@ -0,0 +1,27 @@
+import { Testimonial } from "../../data/Testimonials";
+
+function App(props: Testimonial) {
+  const imgPath = `../../../images/testimonial-${props.photoName}.png`;
+  return (
+    <>
+      <div className="testimonial-container">
+        <img
+          className="testimonial-img"
+          src={imgPath}
+          alt={`${props.name}'s picture`}
+        />
+        <div className="testimonial-text-container">
+          <p className="testimonial-name">
+            <strong>{props.name}</strong> in {props.country}
+          </p>
+          <p className="testimonial-position">
+            {props.position} at <strong>{props.company}</strong>
+          </p>
+          <p className="testimonial-text">{props.testimonial}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
